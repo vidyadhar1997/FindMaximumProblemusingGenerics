c1b23b2ae7f240e6731a4606cac6c8a8b5e51fe3
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-14 15:20:58 +0530

    [vidyadhar hudge]Add:Test case for maximum float number at first poistion

--- MaximumUsingGenricTesting/UnitTest1.cs
+++ MaximumUsingGenricTesting/UnitTest1.cs
@@ -7,10 +7,10 @@ namespace MaximumUsingGenricTesting
     public class UnitTest1
     {
         /// <summary>
-        /// Givens the maximum number when at first position then return same number.
+        /// Givens the maximum number when integer at first position then return same number.
         /// </summary>
         [TestMethod]
-        public void GivenMaxNumber_WhenAtFirstPosition_ThenReturnSameNumber()
+        public void GivenMaxIntegerNumber_WhenAtFirstPosition_ThenReturnSameNumber()
         {
             int expected = 800;
             int maxNumber = MaximumNumberCheck.maximumIntegerNumber(800, 700, 600);
@@ -18,10 +18,10 @@ namespace MaximumUsingGenricTesting
         }
 
         /// <summary>
-        /// Givens the maximum number when at second position then return same number.
+        /// Givens the maximum number when integer at second position then return same number.
         /// </summary>
         [TestMethod]
-        public void GivenMaxNumber_WhenAtSecondPosition_ThenReturnSameNumber()
+        public void GivenMaxIntegerNumber_WhenAtSecondPosition_ThenReturnSameNumber()
         {
             int expected = 800;
             int maxNumber = MaximumNumberCheck.maximumIntegerNumber(700, 800, 600);
@@ -29,13 +29,24 @@ namespace MaximumUsingGenricTesting
         }
 
         /// <summary>
-        /// Givens the maximum number when at second position then return same number.
+        /// Givens the maximum number when integer at second position then return same number.
         /// </summary>
         [TestMethod]
-        public void GivenMaxNumber_WhenAtThirdPosition_ThenReturnSameNumber()
+        public void GivenMaxIntegerNumber_WhenAtThirdPosition_ThenReturnSameNumber()
         {
             int expected = 800;
             int maxNumber = MaximumNumberCheck.maximumIntegerNumber(700, 600, 800);
+            Assert.AreEqual(expected, maxNumber);
+        }
+
+        /// <summary>
+        /// Givens the maximum number is float when at first position then return same number.
+        /// </summary>
+        [TestMethod]
+        public void GivenMaxFloatNumber_WhenAtFirstPosition_ThenReturnSameNumber()
+        {
+            double expected = 70.0;
+            double maxNumber = MaximumNumberCheck.maximumFloateNumber(70.0,60.0,50.0);
             Assert.AreEqual(expected, maxNumber);
         }
     }
