425830c469ed35ab0a0c85720d0a5dcfc90f6a53
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-14 13:53:12 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ FindMaximumUsingGenric/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMaximumUsingGenric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Find Maximum Using Genric Program");
+        }
+    }
+}
