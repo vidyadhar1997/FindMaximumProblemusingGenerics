68f9abf36fb628981278434de88aeef58a89a40e
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-14 16:40:16 +0530

    [vidyadhar hudge]Refactor:Genric methode to find maximum among all the data types

--- FindMaximumUsingGenric/MaximumNumberCheck.cs
+++ FindMaximumUsingGenric/MaximumNumberCheck.cs
@@ -80,6 +80,38 @@ namespace FindMaximumUsingGenric
             }
             throw new Exception("firstString,secondString,thirdString are same");
         }
+
+        /// <summary>
+        /// Finds maximum is genric methode to find max number for integer,string,float values.
+        /// </summary>
+        /// <typeparam name="T"> T is Genric data type of int, float, string</typeparam>
+        /// <param name="firstValue">The first value.</param>
+        /// <param name="secondValue">The second value.</param>
+        /// <param name="thirdValue">The third value.</param>
+        /// <returns>maximum int number/maximum float number/maximum string</returns>
+        /// <exception cref="Exception">firstValue,secondValue,thirdValue are same</exception>
+        public static T findMaximum<T>(T firstValue,T secondValue,T thirdValue) where T : IComparable
+        {
+            if(firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue,secondValue,thirdValue are same");
+        }
     }
 }
 
