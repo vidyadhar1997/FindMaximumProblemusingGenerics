50de0d7399ce6f45cb4fcdf259abe60ba3d1dfec
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-14 15:06:08 +0530

    [vidyadhar hudge]Add:Test case for maximum number at third poistion

--- MaximumUsingGenricTesting/UnitTest1.cs
+++ MaximumUsingGenricTesting/UnitTest1.cs
@@ -16,11 +16,26 @@ namespace MaximumUsingGenricTesting
             int maxNumber = MaximumNumberCheck.maximumIntegerNumber(800, 700, 600);
             Assert.AreEqual(expected, maxNumber);
         }
+
+        /// <summary>
+        /// Givens the maximum number when at second position then return same number.
+        /// </summary>
         [TestMethod]
         public void GivenMaxNumber_WhenAtSecondPosition_ThenReturnSameNumber()
         {
             int expected = 800;
             int maxNumber = MaximumNumberCheck.maximumIntegerNumber(700, 800, 600);
+            Assert.AreEqual(expected, maxNumber);
+        }
+
+        /// <summary>
+        /// Givens the maximum number when at second position then return same number.
+        /// </summary>
+        [TestMethod]
+        public void GivenMaxNumber_WhenAtThirdPosition_ThenReturnSameNumber()
+        {
+            int expected = 800;
+            int maxNumber = MaximumNumberCheck.maximumIntegerNumber(700, 600, 800);
             Assert.AreEqual(expected, maxNumber);
         }
     }
